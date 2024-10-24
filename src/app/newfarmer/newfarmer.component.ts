abdb941a7ef71a4c0b8f4ac0f5bbb372b68c88bf
ugfire <ugfirecompany@gmail.com>
2024-10-24 13:45:10 +0300

    fixed form binding

--- src/app/newfarmer/newfarmer.component.ts
+++ src/app/newfarmer/newfarmer.component.ts
@@ -57,64 +57,80 @@ export class NewfarmerComponent {
   sendFarmerProfileUrl: string =
     'https://test.profiles.symos.asigmagroup.com/api/v1/farmers';
 
-  submitFarmerInfo(): void {
-    try {
-      Swal.fire({
-       
-        html: `
-        <div class="row">
-         <div class="col-2">   <div class="m-1 smallicon rounded-2">
-                    <span class="bi bi-person-fill text-white"></span>
-                  </div></div>
-          <div class="col-10">
-          <h3>FARMER NAME</h3>
-<p><i class="bi bi-telephone-fill me-3"></i> +256..... ${this.farmerprofile.primaryTelephone}  </p>
-          </div></div>
-            
-          <div class="row my-1">
-          <div class="col-6" ></div>
-          <div class="col-6" > <a class="btn asigmabutton"><i class="bi bi-pencil me-3"></i>  Edit</a>
-          <a class="btn btn-danger"><i class="bi bi-trash me-3"></i>  </a>  </div>
-          </div>
-          <table class="table table-bordered">
-           
-          
-              <tr>
-                <th>Gender</th>
-                <td>${this.farmerprofile.gender}</td>
-              </tr>
-              <tr>
-                <th>Age</th>
-                <td>${this.calculateAge(this.farmerprofile.yearOfBirth)}</td>
-              </tr>
-                  <tr>
-                <th>District</th>
-                <td>${this.farmerprofile.district}</td>
-              </tr>
-         
-              <tr>
-                <th>NIN</th>
-                <td>${this.farmerprofile.nationalIdNumber}</td>
-              </tr>
-            </table>
-        `,
-        confirmButtonText: 'OK',
-        showCloseButton: true,
-        showConfirmButton:false,
-        allowOutsideClick: false,
-        customClass: {
-          popup: 'swal-right-position'
-        }
-      });
-      console.log(`Farmer Name: ${this.farmerprofile.firstname} ${this.farmerprofile.lastname}`);
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error Submitting Farmer Profile',
-        text: `${error}`,
-      });
+    captureNewFarmerProfile():void{
+
     }
-  }
+
+    submitFarmerInfo(): void {
+      try {
+        // Check if all fields have values before displaying the SweetAlert
+        if (this.farmerprofile.firstname && this.farmerprofile.lastname && this.farmerprofile.primaryTelephone) {
+          Swal.fire({
+            html: `
+              <div class="row">
+                <div class="col-2">
+                  <div class="m-1 smallicon rounded-2">
+                    <span class="bi bi-person-fill text-white"></span>
+                  </div>
+                </div>
+                <div class="col-10">
+                  <h3>${this.farmerprofile.firstname}</h3>
+                  <p><i class="bi bi-telephone-fill me-3"></i> ${this.farmerprofile.primaryTelephone}</p>
+                </div>
+              </div>
+              
+              <div class="row my-1">
+                <div class="col-6"></div>
+                <div class="col-6">
+                  <a class="btn asigmabutton"><i class="bi bi-pencil me-3"></i> Edit</a>
+                  <a class="btn btn-danger"><i class="bi bi-trash me-3"></i> </a>
+                </div>
+              </div>
+              
+              <table class="table table-bordered">
+                <tr>
+                  <th>Gender</th>
+                  <td>${this.farmerprofile.gender}</td>
+                </tr>
+                <tr>
+                  <th>Age</th>
+                  <td>${this.calculateAge(this.farmerprofile.yearOfBirth)}</td>
+                </tr>
+                <tr>
+                  <th>District</th>
+                  <td>${this.farmerprofile.district}</td>
+                </tr>
+                <tr>
+                  <th>NIN</th>
+                  <td>${this.farmerprofile.nationalIdNumber}</td>
+                </tr>
+              </table>
+            `,
+            confirmButtonText: 'OK',
+            showCloseButton: true,
+            showConfirmButton: false,
+            allowOutsideClick: false,
+            customClass: {
+              popup: 'swal-right-position',
+            },
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Missing Required Fields',
+            text: 'Please fill out all required fields before submitting.',
+          });
+        }
+    
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error Submitting Farmer Profile',
+          text: `${error}`,
+        });
+      }
+    }
+    
 
   calculateAge(yearOfBirth: string): number {
     const currentYear = new Date().getFullYear();
